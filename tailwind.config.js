0509cac9ba34ea01589eec90797827e90aa9d691
Kevin Huang <qw820301@gmail.com>
2024-09-16 17:15:07 +0800

    feat: 統一設定顏色名稱(初版)。
    因應新需求做調整：
     統一設定顏色名稱。
    
    調整項目：
    1. tailwind.config.js，統一設定顏色名稱。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./pages/**/*.html', './layout/**/*.ejs', './main.js'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#69F0AE',
-      },
-    },
-  },
-  plugins: [require('@tailwindcss/forms')],
+	content: ["./pages/**/*.html", "./layout/**/*.ejs", "./main.js"],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: "#27337B",
+					1: "#27337B",
+					2: "#3E4D9C",
+					3: "#7782B8",
+					4: "#C3C8E1",
+				},
+				secondary: {
+					DEFAULT: "#EF4323",
+					1: "#EF4323",
+					2: "#F26348",
+					3: "#F6927F",
+				},
+				highlight: {
+					DEFAULT: "#FFEE75",
+				},
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/forms")],
 };
