8418dddd3d0e771c287e47845238054405c18a0c
Kevin Huang <qw820301@gmail.com>
2024-09-16 23:27:22 +0800

    feat: 新增字體設定的通用類別。
    因應需求做調整：
    1. 依據設計稿新增字體設定的通用類別。
    
    調整項目：
    1. tailwind.config.js，依據設計稿新增字體設定的通用類別。

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,101 @@ export default {
 					'"Noto Color Emoji"',
 				],
 			},
+
+			fontSize: {
+				// H1 到 H7 通用類別樣式設定
+				h1: [
+					"64px",
+					{
+						lineHeight: "1.2",
+						letterSpacing: "0.04em",
+						fontWeight: "700",
+					},
+				],
+				h2: [
+					"56px",
+					{
+						lineHeight: "1.2",
+						letterSpacing: "0.04em",
+						fontWeight: "700",
+					},
+				],
+				h3: [
+					"40px",
+					{
+						lineHeight: "1.2",
+						letterSpacing: "0.04em",
+						fontWeight: "700",
+					},
+				],
+				h4: [
+					"32px",
+					{
+						lineHeight: "1.2",
+						letterSpacing: "0.04em",
+						fontWeight: "700",
+					},
+				],
+				h5: [
+					"28px",
+					{
+						lineHeight: "1.2",
+						letterSpacing: "0.04em",
+						fontWeight: "700",
+					},
+				],
+				h6: [
+					"24px",
+					{
+						lineHeight: "1.2",
+						letterSpacing: "0.04em",
+						fontWeight: "700",
+					},
+				],
+				h7: [
+					"20px",
+					{
+						lineHeight: "1.2",
+						letterSpacing: "0.04em",
+						fontWeight: "700",
+					},
+				],
+
+				// fs-1 到 fs-10 通用類別樣式設定
+				...(() => {
+					const sizes = {};
+					const lineHeight = "1.5";
+					const letterSpacing = "0.02em";
+
+					// 定義 fs-1 到 fs-10 的字體大小
+					const fontSizes = {
+						"fs-1": "40px",
+						"fs-2": "32px",
+						"fs-3": "28px",
+						"fs-4": "24px",
+						"fs-5": "20px",
+						"fs-6": "16px",
+						"fs-7": "14px",
+						"fs-8": "12px",
+						"fs-9": "8px",
+						"fs-10": "6px",
+					};
+
+					// 為每個字體大小設置固定的行高和字母間距
+					for (const [key, size] of Object.entries(fontSizes)) {
+						sizes[key] = [
+							size,
+							{
+								lineHeight: lineHeight,
+								letterSpacing: letterSpacing,
+							},
+						];
+					}
+
+					return sizes;
+				})(),
+			},
+
 			colors: {
 				primary: {
 					DEFAULT: "#27337B",
@@ -33,6 +128,11 @@ export default {
 					DEFAULT: "#FFEE75",
 				},
 			},
+		},
+
+		container: {
+			center: true,
+			padding: "12px",
 		},
 	},
 	plugins: [require("@tailwindcss/forms")],
