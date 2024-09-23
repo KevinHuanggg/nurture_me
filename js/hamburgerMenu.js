a7d7bb9c0bb4815eaca2d13858ec00fd06d9ef56
Maojin-juan <edwardelric199887@gmail.com>
2024-09-24 00:47:13 +0800

    feat：完成header.ejs
    因應新需求做調整：
     完善漢堡選單及清單的hover互動。
    
    調整項目：
    1. 漢堡選單的清單顯示及hover互動效果。
    2. 電腦版各頁面連結之hover互動效果。
    3. header佔據版面之高度調整。
    4. 漢堡的js設定寬度超越1280便關閉選單。

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,107 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const hamburgerMenu = document.getElementById("hamburgerMenu");
+	const hamburgerList = document.getElementById("hamburgerList");
 	let isMenuOpen = false;
 
 	hamburgerMenu.addEventListener("click", () => {
 		isMenuOpen = !isMenuOpen;
 		hamburgerMenu.classList.toggle("hamburgerOpen", isMenuOpen);
 
-		if (hamburgerMenu.classList.contains("h-0")) {
-			searchMember.classList.remove("h-0");
-			searchMember.classList.add("h-[260px]");
-			searchAccordionWho.classList.add(
-				"rounded-b-none",
-				"border-b",
-				"border-neutral-100"
-			);
-			searchMemberTitle.textContent = "Who’s going?";
-			searchMemberSelected.textContent = "1 Adult";
+		if (isMenuOpen) {
+			hamburgerList.style.height = `${hamburgerList.scrollHeight}px`;
 		} else {
-			searchMember.classList.remove("h-[260px]");
-			searchMember.classList.add("h-0");
-			searchMember.addEventListener("transitionend", handleTransitionEnd);
-			searchMemberTitle.textContent = "Who";
-			searchMemberSelected.textContent = "Only me";
-		}
-	});
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-	const searchDatepicker = document.getElementById("searchDatepicker");
-	const searchDatepickerTitle = document.querySelector(
-		".searchDatepicker-title"
-	);
-	const searchDatepickerSelected = document.querySelector(
-		".searchDatepicker-selected"
-	);
-
-	const searchAccordionWho = document.getElementById("searchAccordion-who");
-	const searchMember = document.getElementById("searchMember");
-	const searchMemberTitle = document.querySelector(".searchMember-title");
-	const searchMemberSelected = document.querySelector(
-		".searchMember-selected"
-	);
-
-	const removeTransitionStyles = element => {
-		element.classList.remove(
-			"rounded-b-none",
-			"border-b",
-			"border-neutral-100"
-		);
-	};
-
-	const handleTransitionEnd = event => {
-		if (event.propertyName === "height") {
-			removeTransitionStyles(event.target.previousElementSibling);
-			event.target.removeEventListener(
-				"transitionend",
-				handleTransitionEnd
-			);
-		}
-	};
-
-	searchDropdownWhen.addEventListener("click", () => {
-		if (searchDatepicker.classList.contains("h-0")) {
-			searchDatepicker.classList.remove("h-0");
-			searchDatepicker.classList.add("h-[334px]");
-			searchDropdownWhen.classList.add(
-				"rounded-b-none",
-				"border-b",
-				"border-neutral-100"
-			);
-			searchDatepickerTitle.textContent = "When’s the trip?";
-			searchDatepickerSelected.textContent = "Aug 1-3";
-			searchDatepickerSelected.classList.remove("text-sm");
-		} else {
-			searchDatepicker.classList.remove("h-[334px]");
-			searchDatepicker.classList.add("h-0");
-			searchDatepicker.addEventListener(
-				"transitionend",
-				handleTransitionEnd
-			);
-			searchDatepickerTitle.textContent = "When";
-			searchDatepickerSelected.textContent = "From tomorrow";
-			searchDatepickerSelected.classList.add("text-sm");
+			hamburgerList.style.height = "0";
 		}
 	});
 
-	searchAccordionWho.addEventListener("click", () => {
-		if (searchMember.classList.contains("h-0")) {
-			searchMember.classList.remove("h-0");
-			searchMember.classList.add("h-[260px]");
-			searchAccordionWho.classList.add(
-				"rounded-b-none",
-				"border-b",
-				"border-neutral-100"
-			);
-			searchMemberTitle.textContent = "Who’s going?";
-			searchMemberSelected.textContent = "1 Adult";
-		} else {
-			searchMember.classList.remove("h-[260px]");
-			searchMember.classList.add("h-0");
-			searchMember.addEventListener("transitionend", handleTransitionEnd);
-			searchMemberTitle.textContent = "Who";
-			searchMemberSelected.textContent = "Only me";
+	window.addEventListener("resize", () => {
+		if (window.innerWidth >= 1280) {
+			hamburgerList.style.height = "0";
+			hamburgerMenu.classList.remove("hamburgerOpen");
 		}
 	});
 });
