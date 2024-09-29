8d45076212e3c1f07e1bfb499c18c70286b6e9e4
Maojin-juan <edwardelric199887@gmail.com>
2024-09-29 16:15:18 +0800

    fix：修正header
    問題：
    1. header的顯示方式沒有fixed。
    
    原因：
    1. fixed條件設定錯誤。
    
    調整項目：
    1. 調整header的fixed出現時機。

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const hamburgerMenu = document.getElementById("hamburgerMenu");
 	const hamburgerList = document.getElementById("hamburgerList");
+	const navbar = document.getElementById("navbar");
 	let isMenuOpen = false;
 
 	hamburgerMenu.addEventListener("click", () => {
@@ -11,13 +12,22 @@ document.addEventListener("DOMContentLoaded", () => {
 			hamburgerList.style.height = `${hamburgerList.scrollHeight}px`;
 		} else {
 			hamburgerList.style.height = "0";
+			navbar.classList.remove("fixed");
 		}
 	});
 
 	window.addEventListener("resize", () => {
-		if (window.innerWidth >= 1280) {
+		if (window.innerWidth >= 1200) {
 			hamburgerList.style.height = "0";
 			hamburgerMenu.classList.remove("hamburgerOpen");
+		}
+	});
+
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 0 && window.innerWidth < 1200) {
+			navbar.classList.add("fixed");
+		} else if (window.scrollY <= 0 && window.innerWidth < 1200) {
+			navbar.classList.remove("fixed");
 		}
 	});
 });
