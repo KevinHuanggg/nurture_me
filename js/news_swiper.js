d981bb1824d4f77c057ac601e97879ba004ba1d8
Shiloh <s1042952@gm.pu.edu.tw>
2024-10-01 13:54:02 +0800

    fix:救回我的banner swiper 移除main.css flex 跟 flex-col

--- js/news_swiper.js
+++ js/news_swiper.js
@@ -1,19 +1,25 @@
-import { Autoplay } from "swiper/modules";
+import Swiper from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 var swiper = new Swiper(".mySwiper", {
-    loop: true,  // 讓輪播循環
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '" style="width: 16px; height: 16px; background-color: #7782B8;"></span>';
-      }
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    //autoplay:{
-      //delay: 4000,
-    //}
-  });
+  modules: [Pagination, Navigation, Autoplay],  // 啟用所需模塊
+  loop: true,  // 讓輪播循環
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '" style="width: 16px; height: 16px; background-color: #7782B8;"></span>';
+    }
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //autoplay: {
+  //  delay: 4000,
+  //}
+});
